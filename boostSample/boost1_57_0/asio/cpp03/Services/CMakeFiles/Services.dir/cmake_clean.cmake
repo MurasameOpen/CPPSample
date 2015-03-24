2a78dce0db052b2649bdb1657cc24cbfe8df9616
yuki <yukimariamiyatake@gmail.com>
2015-03-25 01:25:14 +0900

    no message

---
+++ boostSample/boost1_57_0/asio/cpp03/Services/CMakeFiles/Services.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Services.dir/daytime_client.cpp.o"
+  "CMakeFiles/Services.dir/logger_service.cpp.o"
+  "bin/Services.pdb"
+  "bin/Services"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Services.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ boostSample/boost1_57_0/asio/cpp11/allocation/CMakeFiles/allocation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/allocation.dir/server.cpp.o"
+  "bin/allocation.pdb"
+  "bin/allocation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/allocation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
